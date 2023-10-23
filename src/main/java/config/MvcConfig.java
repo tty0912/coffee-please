7ffddefb16f0efdecd2d3a3a09cebd686dcb0ff4
EunsikP <137468570+EunsikP@users.noreply.github.com>
2023-10-23 15:13:44 +0900

    update Config, BuyerDO/DAO, SellerDO/DAO
    수정 및 업데이트

--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package main.java.config;
+package config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
