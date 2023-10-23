0fcaefc106c486551586476b059a3f591508caaa
JHJ <tty0912@gmail.com>
2023-10-23 12:44:16 +0900

    #feat BuyerDO

---
+++ src/main/java/model/member/BuyerDO.java
@@ -0,0 +1,86 @@
+package main.java.model.member;
+
+public class BuyerDO {
+
+    private String buyerEmail;
+    private String buyerName;
+    private String nickname;
+    private String passwd;
+    private int point;
+    private int tel;
+    private String regdate;
+    private String buyerImg;
+    private String address;
+
+    public String getBuyerEmail() {
+        return buyerEmail;
+    }
+
+    public void setBuyerEmail(String buyerEmail) {
+        this.buyerEmail = buyerEmail;
+    }
+
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    public void setBuyerName(String buyerName) {
+        this.buyerName = buyerName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public String getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(String regdate) {
+        this.regdate = regdate;
+    }
+
+    public String getBuyerImg() {
+        return buyerImg;
+    }
+
+    public void setBuyerImg(String buyerImg) {
+        this.buyerImg = buyerImg;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
