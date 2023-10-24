43248041b7484b74809c28b08537bf638f867013
hyeonjoon <tty0912@gmail.com>
2023-10-25 01:27:00 +0900

    #feat package 수정

--- src/main/java/config/ControllerConfig.java
+++ src/main/java/config/ControllerConfig.java
@@ -1,4 +1,4 @@
-package config;
+package main.java.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package config;
+package main.java.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
