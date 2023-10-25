1a08ba1b42cf9ade59c835403b8048d9857eacb9
9a_orn <Lum.Gayoung@gmail.com>
2023-10-25 14:15:10 +0900

    컨트롤러 페이지이동

--- src/main/java/config/ControllerConfig.java
+++ src/main/java/config/ControllerConfig.java
@@ -3,6 +3,13 @@ package main.java.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import controller.MemberController;
+
+@Configuration
 public class ControllerConfig {
 
+	@Bean
+	public MemberController memberController() {
+		return new MemberController();
+	}
 }
