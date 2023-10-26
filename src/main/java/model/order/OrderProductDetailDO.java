9d70e734b234228f8e256218b7a7f8c249a65f02
JHJ <tty0912@gmail.com>
2023-10-26 17:06:35 +0900

    #feat OrderProductDetailDO

--- src/main/java/model/order/OrderProductDetailDO.java
+++ src/main/java/model/order/OrderProductDetailDO.java
@@ -5,5 +5,37 @@ public class OrderProductDetailDO {
     private String buyerEmail;
     private String orderDatetime;
     private long beansNum;
-    private long orderTotalPrice;
+    private int qty;
+
+    public String getBuyerEmail() {
+        return buyerEmail;
+    }
+
+    public void setBuyerEmail(String buyerEmail) {
+        this.buyerEmail = buyerEmail;
+    }
+
+    public String getOrderDatetime() {
+        return orderDatetime;
+    }
+
+    public void setOrderDatetime(String orderDatetime) {
+        this.orderDatetime = orderDatetime;
+    }
+
+    public long getBeansNum() {
+        return beansNum;
+    }
+
+    public void setBeansNum(long beansNum) {
+        this.beansNum = beansNum;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
 }
