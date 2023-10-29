f3f6c59ddb2b69632e283e98b36db02c5369a733
hyeonjoon <tty0912@gmail.com>
2023-10-29 19:32:36 +0900

    #feat OrderService

--- src/main/java/model/service/OrderService.java
+++ src/main/java/model/service/OrderService.java
@@ -1,6 +1,25 @@
 package main.java.model.service;
 
+import main.java.model.member.BuyerDAO;
+import main.java.model.member.SellerDAO;
+import main.java.model.order.OrderProductDO;
+import org.springframework.stereotype.Service;
+
 public class OrderService {
 
+    BuyerDAO buyerDAO = new BuyerDAO();
+    SellerDAO sellerDAO = new SellerDAO();
 
+
+//    public void onlyOnePayment(int beansNum, int qyt, String buyerEmail) {
+//
+//        long point = buyerDAO.checkBuyerPoint(buyerEmail);
+//
+//        //구매자 포인트 차감 + 판매자 포인트 증가
+//        if (point >= beansPrice * qyt){
+//            buyerDAO.buyerPointUpdate(buyerEmail,beansPrice*qyt);
+//            sellerDAO.sellerPointUpdate()
+//        }
+//        //
+//    }
 }
